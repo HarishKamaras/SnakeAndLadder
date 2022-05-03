257e346613d2894eb4b5b8401443496a729a9862
HarishKamaras <harishkamarasu@gmail.com>
2022-05-03 17:10:39 +0530

    Added : PlayerPosition

--- src/com/brigdelabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/brigdelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -1,6 +1,8 @@
 package com.brigdelabz.SnakeAndLadder;
 
 public class SnakeAndLadder {
+    static int START_POSITION = 0;
+    static int PLAYER_POSITION = 0;
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder game");
     }
