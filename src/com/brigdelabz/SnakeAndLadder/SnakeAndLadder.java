d13bdfddb879b4e0739006f3f5e7626a8f498267
HarishKamaras <harishkamarasu@gmail.com>
2022-05-03 17:00:07 +0530

    Added : Snake and Ladder

---
+++ src/com/brigdelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -0,0 +1,8 @@
+package com.brigdelabz.SnakeAndLadder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake and ladder game");
+    }
+}
+
