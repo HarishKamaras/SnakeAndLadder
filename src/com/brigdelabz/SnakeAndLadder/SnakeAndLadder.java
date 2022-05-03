1db4673b86bfaee92a5746286560c79d7de61b04
HarishKamaras <harishkamarasu@gmail.com>
2022-05-03 17:18:26 +0530

    Added : DieRoll

--- src/com/brigdelabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/brigdelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -3,8 +3,13 @@ package com.brigdelabz.SnakeAndLadder;
 public class SnakeAndLadder {
     static int START_POSITION = 0;
     static int PLAYER_POSITION = 0;
+    private static int dieRoll() {
+        int dieValue = (int) (Math.random() * 10 % 6) + 1;
+        return dieValue;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder game");
+        System.out.println("Die : " +dieRoll());
     }
 }
 
